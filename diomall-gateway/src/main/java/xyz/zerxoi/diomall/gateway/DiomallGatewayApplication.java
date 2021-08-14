bf8c73adc45846cba3595b9e5951761eebedbcfa
zouxin <zouxin@kuaishou.com>
2021-08-15 02:23:24 +0800

    gateway 网关

---
+++ diomall-gateway/src/main/java/xyz/zerxoi/diomall/gateway/DiomallGatewayApplication.java
@@ -0,0 +1,15 @@
+package xyz.zerxoi.diomall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DiomallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiomallGatewayApplication.class, args);
+    }
+
+}
---
+++ diomall-gateway/src/test/java/xyz/zerxoi/diomall/gateway/DiomallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.zerxoi.diomall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiomallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
