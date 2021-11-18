3f6449ca9e76c5d30bfdc780acf55d37b0ac7363
zerxoi <zerxoi@163.com>
2021-11-18 10:59:40 +0800

    订单动静分离

---
+++ diomall-order/src/main/java/xyz/zerxoi/diomall/order/web/TemplateController.java
@@ -0,0 +1,13 @@
+package xyz.zerxoi.diomall.order.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class TemplateController {
+    @GetMapping("/{template}.html")
+    public String templateHtml(@PathVariable("template") String template) {
+        return template;
+    }
+}
